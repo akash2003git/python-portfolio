af4137e1c1807870f1d3b4625da82fb5e35c87f2
Akash <akash.tayade.dev@gmail.com>
2023-11-12 11:06:53 +0530

    Add description on the new columns

--- main.py
+++ main.py
@@ -9,10 +9,15 @@ with col1:
 
 with col2:
     st.title("Akash Tayade")
-    content = """
+    content1 = """
     Hi, I am Akash! I am a beginner Python programmer and a student.
      I am currently pursuing Btech. in Electronics Engineering at YCCE Nagpur.
      This is my portfolio website to showcase my Python projects. 
      (Also, this is my dog, his name is Jimmy and he's an Indian Spitz.)
     """
-    st.info(content)
+    st.info(content1)
+
+content2 = """Below you can find some of the apps I have built in Python. 
+              Feel free to contact me!"""
+
+st.write(content2)
